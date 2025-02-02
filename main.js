0cb6468461dd5e1a23430ac2f1f8ba6423650672
WyattCPhT <113256632+WyattCPhT@users.noreply.github.com>
2025-02-01 21:10:17 -0800

    Update main.js

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ $(() => {
 
             let explorerInfo;
             if(!$masters.is(":checked"))
-                explorerInfo = await fetch(`https://explorer.lichess.ovh/masters?fen=${encodeURIComponent(game.fen())}`)
+                explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?fen=${encodeURIComponent(game.fen())}`)
                 .then(response => response.json());
             else
                 explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?fen=${encodeURIComponent(game.fen())}`)
@@ -126,30 +126,36 @@ $(() => {
             $queryInfo.text("");
 
             sortedQueryInfo = queryInfo.map(obj => ({ ...obj }));
-            sortedQueryInfo.sort((a, b) => {
-                let aComparisonValue;
-                let bComparisonValue;
-                switch (game.turn() === "w" ? $whiteSortOptions.val() : $blackSortOptions.val()) {
-                    case "accuracy":
-                        aComparisonValue = a.score;
-                        bComparisonValue = b.score;
-                        break;
-                    case "popularity":
-                        aComparisonValue = a.total;
-                        bComparisonValue = b.total;
-                        break;
-                    case "winrate":
-                        aComparisonValue = a.winrate;
-                        bComparisonValue = b.winrate;
-                        break;
-                }
-                if (isNaN(aComparisonValue) && isNaN(bComparisonValue)) return 1;
-                if (isNaN(aComparisonValue)) return 1;
-                if (isNaN(bComparisonValue)) return -1;
-                if (aComparisonValue === bComparisonValue) return b.total - a.total;
-                if (game.turn() === "b" && $blackSortOptions.val() === "winrate") return aComparisonValue - bComparisonValue;
-                return bComparisonValue - aComparisonValue;
-            });
+    sortedQueryInfo.sort((a, b) => {
+        let aComparisonValue;
+        let bComparisonValue;
+    switch (game.turn() === "w" ? $whiteSortOptions.val() : $blackSortOptions.val()) {
+        case "accuracy":
+            aComparisonValue = a.score;
+            bComparisonValue = b.score;
+            break;
+        case "popularity":
+            aComparisonValue = a.total;
+            bComparisonValue = b.total;
+            break;
+        case "winrate":
+            aComparisonValue = a.winrate;
+            bComparisonValue = b.winrate;
+            break;
+    }
+    if (isNaN(aComparisonValue) && isNaN(bComparisonValue)) return 1;
+    if (isNaN(aComparisonValue)) return 1;
+    if (isNaN(bComparisonValue)) return -1;
+    if (aComparisonValue === bComparisonValue) {
+        if (a.total === b.total) {
+            return queryInfo.indexOf(a) - queryInfo.indexOf(b); // Preserve original order from ChessDB
+        } else {
+            return b.total - a.total; // Sort by total positions
+        }
+    }
+    if (game.turn() === "b" && $blackSortOptions.val() === "winrate") return aComparisonValue - bComparisonValue;
+    return bComparisonValue - aComparisonValue;
+});
 
             for (let i = 0; i < sortedQueryInfo.length; i++) {
                 if (sortedQueryInfo[i].moveSan === undefined) continue;
