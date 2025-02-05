5bdef09c50478d618e2a09207d6fc6a6b1d8a30e
WyattCPhT <113256632+WyattCPhT@users.noreply.github.com>
2025-02-05 12:17:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -89,10 +89,10 @@ $(() => {
 
             let explorerInfo;
             if(!$masters.is(":checked"))
-                explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?ratings=0&fen=${encodeURIComponent(game.fen())}`)
+                explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?speeds=bullet&fen=${encodeURIComponent(game.fen())}`)
                 .then(response => response.json());
             else
-                explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?ratings=0&fen=${encodeURIComponent(game.fen())}`)
+                explorerInfo = await fetch(`https://explorer.lichess.ovh/lichess?speeds=bullet&fen=${encodeURIComponent(game.fen())}`)
                 .then(response => response.json());
 
             let white = explorerInfo.white;
